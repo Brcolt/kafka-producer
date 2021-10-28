5f3dc6ed887e881776bc0a29c2c3f66038fec805
Bruno Colt <bruno.nascimento@squadra.com.br>
2021-10-28 12:51:17 -0300

    Initial commit

---
+++ src/main/java/controller/KafkaController.java
@@ -0,0 +1,21 @@
+package controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import producer.TopicProducer;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final TopicProducer topicProducer;
+
+    @GetMapping(value = "/send")
+    public void send() {
+        topicProducer.send("Mensagem de teste enviada ao topico");
+    }
+}
